dd7f426316156aa439f0793a12c337c602900039
Van Nguyen <nguyentvvan@gmail.com>
2023-03-30 19:08:39 +0700

    adding animation for moments

--- components/GreatestMoments.js
+++ components/GreatestMoments.js
@@ -26,14 +26,14 @@ export default function GreatestMoments() {
 			</h3>
 
 			<div className={styles.greatestMoments}>
-				<img className={styles.a} src={getImageURL(images[0])} alt='greatest moments' />
-				<img className={styles.b} src={getImageURL(images[1])} alt='greatest moments' />
-				<img className={styles.c} src={getImageURL(images[2])} alt='greatest moments' />
-				<img className={styles.d} src={getImageURL(images[3])} alt='greatest moments' />
-				<img className={styles.e} src={getImageURL(images[4])} alt='greatest moments' />
-				<img className={styles.f} src={getImageURL(images[5])} alt='greatest moments' />
-				<img className={styles.g} src={getImageURL(images[6])} alt='greatest moments' />
-				<img className={styles.h} src={getImageURL(images[7])} alt='greatest moments' />
+				<img className={clsx("revealLeft", styles.a)} src={getImageURL(images[0])} alt='greatest moments' />
+				<img className={clsx("revealLeft", styles.b)} src={getImageURL(images[1])} alt='greatest moments' />
+				<img className={clsx("revealRight", styles.c)} src={getImageURL(images[2])} alt='greatest moments' />
+				<img className={clsx("revealRight", styles.d)} src={getImageURL(images[3])} alt='greatest moments' />
+				<img className={clsx("revealLeft", styles.e)} src={getImageURL(images[4])} alt='greatest moments' />
+				<img className={clsx("revealLeft", styles.f)} src={getImageURL(images[5])} alt='greatest moments' />
+				<img className={clsx("revealRight", styles.g)} src={getImageURL(images[6])} alt='greatest moments' />
+				<img className={clsx("revealRight", styles.h)} src={getImageURL(images[7])} alt='greatest moments' />
 			</div>
 		</div>
 	)
